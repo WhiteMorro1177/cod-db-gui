8f072ec1dbbf1067ee68490f29690aeb441ad030
WhiteMorro1177 <jackforest228@gmail.com>
2023-12-25 13:54:31 +0300

    final

--- COD_UI/Data.cs
+++ COD_UI/Data.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using COD_UI.DTO;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace COD_UI
@@ -11,17 +13,13 @@ namespace COD_UI
 		// defults
 		public const string TB_USERNAME_DEFAULT_VALUE = "Enter your username";
 		public const string TB_PASSWORD_DEFAULT_VALUE = "Enter your password";
-		public const string TB_CLIENT_PASSPORT_DETAILS_DEFAULT_VALUE = "1234 123456";
-		public const string TB_CLIENT_PHONE_NUMBER_DEFAULT_VALUE = "+7 (916) 123-45-67";
 
 
-		// window settings
-		public const int WINDOW_WIDTH_DEFAULT = 448;
-		public const int WINDOW_WIDTH_EXTENDED = 758;
 
-		public const int RESULT_SPLITTER_DISTANCE = 434;
+		// app settings
+		public static string USERNAME = null;
 
 		// connection settings
-
+		public static RestClient client = new RestClient("http://localhost:3002");
 	}
 }
